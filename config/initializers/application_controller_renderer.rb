bb2362a0340e3ce9b56cf1f34796250ac575bfec
Geoglyph000 <nasca19970911@gmail.com>
2021-07-14 02:45:08 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f528e51cb211c34afa3bc69e49ac5dfcf7e9ef936c6944ba3cc1301bedf48eacc09bd1a94322a0898d800937c025ab0c0e3ccd3838de9c830d1e86d0832f938b'
